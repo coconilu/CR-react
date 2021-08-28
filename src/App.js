6279f88d81494643dac0fd660e2b249560361ff4
Bayes <bayes0621@gmail.com>
2021-08-28 12:45:08 +0800

    add HelloApp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import HelloApp from "./Widgets/Hello";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <HelloApp />
       </header>
     </div>
   );
